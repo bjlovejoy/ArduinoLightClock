8b97f6b820da59d60c671ea990483bc8d2037d9c
Brendon <bjlovejoy0924@gmail.com>
2019-07-11 20:29:35 -0400

    Adding remote mapping and bitmap changes

--- ArduinoNanoLightClock/ArduinoNanoLightClock.ino
+++ ArduinoNanoLightClock/ArduinoNanoLightClock.ino
@@ -1,49 +1,114 @@
+#include <Wire.h>
+#include <SPI.h>
 #include "TM1637Display.h"
 #include "RTClib.h"
 #include "Adafruit_GFX.h"
 #include "Adafruit_SSD1306.h"
-#include <Wire.h>
+#include "allFunctions.h"
+//#include "bitmapArray.h"
+//#include "remoteCode.h"
+#include "IRremote.h"
 
 //Pins and obeject for the 4-Digit Display module
 int CLK = 7;
 int DIO = 8;
-TM1637Display display(CLK, DIO);
+TM1637Display digitDisplay(CLK, DIO);
 
-//Pins and object for rtc module
-//D -> SDA     //C - SCL     //NC -> No connection -> GND
+//Pins and object for rtc module        //D -> SDA     C - SCL     NC -> No connection -> GND
 RTC_DS3231 rtc;
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 //OLED Display
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
+#define OLED_RESET    -1  // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 
 //LED pins
 int red = 5;
 int green = 6;
 int blue = 9;
 
-int buzzer = 4;
+//Other input and output pins
+int buzzer = 9;
 int temp = 14;
+
+//IR remote and receiver
 int irSensor = 15;
+IRrecv irrecv(irSensor);
+decode_results results;
+unsigned long pushedButton = 0;
+
+int irInput = 0;
+bool error = false;
 
 void setup()
 {
   Serial.begin(9600);
+  delay(2000);
   
   pinMode(red, OUTPUT);
   pinMode(green, OUTPUT);
   pinMode(blue, OUTPUT);
 
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  digitalWrite(red, LOW);
+  digitalWrite(green, LOW);
+  digitalWrite(blue, LOW);
+  //noTone(buzzer);
+  irrecv.enableIRIn(); // Start the receiver
+
+  //Initialize the rtc module
+  if(!rtc.begin())
+  {
+    Serial.println("Couldn't find RTC");
+    error = true;
+    while(true)  // Don't proceed, loop forever
+    {
+      digitalWrite(red, error);
+      error = !error;
+      delay(1000);
+    }
+  }
+
+  //initialize the OLED display and display Logo
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))  // Address 0x3C for 128x64
   {
     Serial.println(F("SSD1306 allocation failed"));
-    digitalWrite(red, HIGH);
-    for(;;); // Don't proceed, loop forever
+    error = true;
+    while(true)  // Don't proceed, loop forever
+    {
+      digitalWrite(red, error);
+      error = !error;
+      delay(200);
+    }
   }
+  display.clearDisplay();
+  display.drawBitmap(0, 0, lovejoy_inventions_bitmap, 128, 64, 1);
+  display.display();
+  delay(2000);
+
+  if(rtc.lostPower())
+  {
+    digitalWrite(blue, HIGH);
+    delay(500);
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setCursor(0, 0);
+    display.cp437(true);
+    display.write("Press OK to enter programming mode.");
+    display.display();
+    while(pushedButton != buttons.OK)
+    {
+      irrecv.decode(&results);
+      pushedButton = results.value;
+    }
+    pushedButton = 0;
+    irrecv.resume();
+    
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
+  
 }
 
 void loop()
--- README.md
+++ README.md
@@ -2,13 +2,30 @@
 Button-less Arduino clock with tri-color LEDs
 
 
-All of the libraries and references used are included below for easy setup.
+All of the libraries and references used for this project are included below for easy setup.
 I also included an over-detailed set-up description below.
 
-**Important Notes:**
-This code was initially written for the Arduino Nano and then converted for the Arduino Pro Micro.  I have included links to the Amazon page I bought them from.  I included code for the Arduino Uno, however it is mostly similar to the code for the Nano.
+Materials:
+Aruidno of choice
+4-Digit 7-Segment Display
+Piezo Buzzer (Passive)
+Temperature and Humidity Module (DHT)
+OLED Display ()
+IR Sensor/Reciever
+Tri-Colored LEDs
+Breadboard/Perf-Board (for prototyping)
+Wires/Jumper-Cables
+Arduino Remote (Elegoo)
+3 100-330 ohm resistors
 
-I also inlucded links to all of the sensors I used and their data sheets (if I could find them).  Some of the libraries/code I used may not be compatible with other sensors, however, I will try to make the documentation as clear as possible so that adjustments and substitutions can be made easily.
+(If you don't have or don't wish to include any of the above modules (such as the DHT module), follow the comments in the main Arduino file to remove them).
+
+
+**Important Notes:**
+This code was initially written for the Arduino Nano and then converted for the Arduino Pro Micro.  I have included links to the Amazon pages I bought them from.  I included code for the Arduino Uno, however it is mostly similar to the code for the Nano.
+
+I also inlucded links to all of the sensors I used and their data sheets (if I could find them).  Some of the libraries/code I used may not be compatible with other sensors, however, I will try to make the documentation as clear as possible so that adjustments and substitutions can be made easily (for example, RTClib.h has example code for other modules if you aren't using the same ds3231 module I used).
+
 
 For Arduino Nano:
 
@@ -51,6 +68,12 @@ Used Adafruit SSD1306 and GFX libraries
 (https://github.com/adafruit/Adafruit_SSD1306)
 (https://github.com/adafruit/Adafruit-GFX-Library)
 Change the i2c address in main file to 3C or 3D
+
+(Links for making custom bitmap:)
+https://www.youtube.com/watch?v=iy89vaeMOPY&list=WL&index=51&t=0s
+http://marlinfw.org/tools/u8glib/converter.html
+https://gist.github.com/ArnauMA/15a9b70538f37dddd3ad32844a634ccb
+
 
 **Infra-red Remote and Receiver:**
 
